66e6a387a2ded6f5461bbd4e3e5cc236c7802186
PierreND <pierre@nativefrenchspeech.com>
2014-03-18 12:41:45 -0400

    Serve JSon

--- fakejson.go
+++ fakejson.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"encoding/json"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 type Profile struct {
@@ -25,18 +27,48 @@ type Piechardata struct {
 type Areachart struct {
 }
 
-func main() {
-	http.HandleFunc("/", foo)
-	http.ListenAndServe(":3000", nil)
+type Tvg struct {
+	Profil              string `json:"profil"`
+	Montantinitial      int    `json:"montantinitial"`
+	Montantdate         int    `json:"montantdate"`
+	Apportsretraits     int    `json:"apportsretraits"`
+	Apportsretraitsdate int    `json:"apportsretraitsdate"`
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
-	js, err := json.Marshal(profile)
+func createtvg() Tvg {
+	p := []string{"Discrétionnaire", "Assurance vie", "Immobilier", "Fonds"}[randInt(0, 4)]
+	m1 := randInt(80000, 90000)
+	m2 := randInt(90000, 100000)
+	a1 := randInt(110000, 120000)
+	a2 := randInt(120000, 130000)
+	return Tvg{Profil: p, Montantinitial: m1, Montantdate: m2, Apportsretraits: a1, Apportsretraitsdate: a2}
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func apis(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var api string = r.URL.Path[len("/json_server/"):]
+	var o interface{}
+	switch api {
+	case "tvg":
+		o = createtvg()
+	default:
+		o = "Error: " + api + " (no api with this name)"
+	}
+	js, err := json.Marshal(o)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
+}
+
+func main() {
+	http.HandleFunc("/", apis)
+	//http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
 }
