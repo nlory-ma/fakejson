8472b7584b58782eb5488eb38a6d7d39cbce84cb
PierreND <pierre@nativefrenchspeech.com>
2014-03-27 20:33:31 -0400

    Adjust values

--- fakejson.go
+++ fakejson.go
@@ -38,37 +38,49 @@ type Tvg struct {
 
 func createtvg() Tvg {
 	p := []string{"Discrétionnaire", "Assurance vie", "Immobilier", "Fonds"}[randInt(0, 4)]
-	m1 := randInt(80000, 90000)
-	m2 := randInt(90000, 100000)
+	m1 := randInt(800000, 900000)
+	m2 := randInt(900000, 1000000)
 	a1 := randInt(110000, 120000)
-	a2 := randInt(120000, 130000)
+	a2 := randInt(1200000, 1300000)
 	return Tvg{Profil: p, Montantinitial: m1, Montantdate: m2, Apportsretraits: a1, Apportsretraitsdate: a2}
 }
 
 type Tvgp struct {
-	Unmoisperf    int `json:"unmoisperf"`
-	Unmoispmv     int `json:"unmoispmv"`
-	Troismoisperf int `json:"troismoisperf"`
-	Troismoispmv  int `json:"troismoispmv"`
-	Unanperf      int `json:"unanperf"`
-	Unanpmv       int `json:"unanpmv"`
-	Troisansperf  int `json:"troisansperf"`
-	Troisanspmv   int `json:"troisanspmv"`
-	Creationperf  int `json:"creationperf"`
-	Creationpmv   int `json:"creationpmv"`
+	Unmoisperf    float64 `json:"unmoisperf"`
+	Unmoispmv     int     `json:"unmoispmv"`
+	Troismoisperf float64 `json:"troismoisperf"`
+	Troismoispmv  int     `json:"troismoispmv"`
+	Unanperf      float64 `json:"unanperf"`
+	Unanpmv       int     `json:"unanpmv"`
+	Troisansperf  float64 `json:"troisansperf"`
+	Troisanspmv   int     `json:"troisanspmv"`
+	Creationperf  float64 `json:"creationperf"`
+	Creationpmv   int     `json:"creationpmv"`
 }
 
 func createtvgp() Tvgp {
-	u1 := randInt(5, 15)
-	u2 := randInt(500, 1500)
-	t1 := randInt(5, 15)
-	t2 := randInt(500, 1500)
-	a1 := randInt(50, 150)
-	a2 := randInt(5000, 15000)
-	r1 := randInt(50, 150)
-	r2 := randInt(5000, 15000)
-	c1 := randInt(500, 1500)
-	c2 := randInt(50000, 150000)
+	u1 := 3.49
+	u2 := 109763
+	t1 := 4.39
+	t2 := 136928
+	a1 := 7.42
+	a2 := 185484
+	r1 := -1.83
+	r2 := 171668
+	c1 := -1.83
+	c2 := 171668
+	/*
+		u1 := randInt(5, 15)
+		u2 := randInt(500, 1500)
+		t1 := randInt(5, 15)
+		t2 := randInt(500, 1500)
+		a1 := randInt(50, 150)
+		a2 := randInt(5000, 15000)
+		r1 := randInt(50, 150)
+		r2 := randInt(5000, 15000)
+		c1 := randInt(500, 1500)
+		c2 := randInt(50000, 150000)
+	*/
 	return Tvgp{Unmoisperf: u1, Unmoispmv: u2, Troismoisperf: t1, Troismoispmv: t2, Unanperf: a1, Unanpmv: a2, Troisansperf: r1, Troisanspmv: r2, Creationperf: c1, Creationpmv: c2}
 }
 
@@ -114,9 +126,9 @@ func apis(w http.ResponseWriter, r *http.Request) {
 	case "avcs":
 		o = createlineactif("Compte VIE", true, "Banque JUMP")
 	case "bus":
-		o = createlineactif("Emprunt", false, "Banque JUMP")
+		o = createlineactif("Emprunt", false, "Direct")
 	case "brs":
-		o = createlineactif("T2 Sentier Paris", false, "Banque JUMP")
+		o = createlineactif("T2 Sentier Paris", false, "SCI")
 	case "pfs":
 		o = createlineactif("SCPI", false, "Banque JUMP")
 	default:
